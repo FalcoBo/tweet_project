6b2fbc873b5ff581e03a6964202d7c4a8361e03d
Theo <118967935+FalcoBo@users.noreply.github.com>
2025-06-28 11:01:00 +0200

    fix preprocessing & add tests

---
+++ tests/test_modeling.py
@@ -0,0 +1,3 @@
+import pytest
+
+from src.modeling import *
---
+++ tests/test_preprocessing.py
@@ -0,0 +1,48 @@
+import pytest
+
+from src.preprocessing import *
+
+def test_preprocess_data():
+    pass
+
+def test_handle_missing_values():
+    pass
+
+def test_remove_duplicates():
+    pass
+
+def test_clean_text():
+    pass
+
+def test_tokenize_text():
+    pass
+
+def test_stem_text():
+    pass
+
+def test_preprocess_text():
+    pass
+
+def test_preprocess_text_column():
+    pass
+
+def test_create_features():
+    pass
+
+def test_load_data():
+    pass
+
+def test_get_text_statistics():
+    pass
+
+def test_identify_outliers():
+    pass
+
+def test_preprocess_text_spacy():
+    pass
+
+def test_extract_named_entities():
+    pass
+
+def test_extract_postags():
+    pass
